d996f7c77e29dd455d8ef64a7b1158d82a0ecb0f
Tzachi Dar <tzachi.dar@gmail.com>
2024-03-11 01:51:20 +0200

    First version, works by sending an intent.

---
+++ app/src/main/java/com/tzachi/mynetworkservice/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.tzachi.mynetworkservice
+
+import android.os.Bundle
+import android.content.Intent
+import android.os.Build
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tzachi.mynetworkservice.ui.theme.MyNetworkServiceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyNetworkServiceTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(Intent(applicationContext, TcpServerService::class.java))
+        } else {
+            startService(Intent(applicationContext, TcpServerService::class.java))
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyNetworkServiceTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/tzachi/mynetworkservice/TcpClientHandler.kt
@@ -0,0 +1,46 @@
+package com.tzachi.mynetworkservice
+
+import android.content.Intent
+import android.util.Log
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.io.IOException
+
+class TcpClientHandler(private val dataInputStream: DataInputStream, private val dataOutputStream: DataOutputStream) : Thread() {
+    override fun run() {
+        while (true) {
+            try {
+                dataInputStream.close()
+                dataOutputStream.close()
+
+                if(dataInputStream.available() > 0){
+                    Log.i(TAG, "Received: " + dataInputStream.readUTF())
+                    dataOutputStream.writeUTF("Hello Client")
+                    sleep(2000L)
+
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+                try {
+                    dataInputStream.close()
+                    dataOutputStream.close()
+                } catch (ex: IOException) {
+                    ex.printStackTrace()
+                }
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+                try {
+                    dataInputStream.close()
+                    dataOutputStream.close()
+                } catch (ex: IOException) {
+                    ex.printStackTrace()
+                }
+            }
+        }
+    }
+
+    companion object {
+        private val TAG = TcpClientHandler::class.java.simpleName
+    }
+
+}
